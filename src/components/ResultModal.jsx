1bfa77bcec4bab92e406a9b6f4c1f0e5bb9e6acd
Steve Tyler <steve.tyler@centrica.com>
2024-05-03 17:18:10 +0100

    section 140

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,19 @@
-import { forwardRef } from 'react'; // forward ref from one component to another
+import { forwardRef, useImperativeHandle, useRef } from 'react'; // forward ref from one component to another
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime}, ref) {
+  const dialog = useRef(); // decouple dialog component from external components
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal(); // standard browser feature
+      }
+    }
+  }); // to define properties and methods that should be accessible on this component from the outside
+
   return (
       // cannot use open attr here as we want to fade background behind dialog
-    <dialog ref={ref} className="result-modal"> 
+    <dialog ref={dialog} className="result-modal"> 
       <h2>You {result}</h2>
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>X seconds</strong> left</p>
